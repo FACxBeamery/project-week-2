1754bd5c84f2caec42e45499384050041a4cf4b4
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-09-18 17:02:44 +0100

    dynamically added pokemon card when input section 1

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 
 // pokeapi 
 
-// window.onload = (event => {
-//     fetch('https://pokeapi.co/api/v2/pokemon/id/', options)
-//         .then(response => response.json())
-//         .then(result => console.log(result))
-// }, false);
-
 (() => { function fn()  {
     // "On document ready" commands:
     const types = [];
@@ -22,7 +16,9 @@
             const newText = document.createTextNode(type);
             newOption.appendChild(newText);
             typeSelector.appendChild(newOption);
+            
           });  
+          // autocomplete(document.getElementById("myInput"), types);
 
           const regions = [];
           const regionSelector = document.querySelector('#regions-selector');
@@ -31,7 +27,7 @@
           regionsXHR.onreadystatechange = () => {
             if (regionsXHR.readyState === 4 && regionsXHR.status === 200) {
               JSON.parse(regionsXHR.response).results.map(result => regions.push(result.name));
-              console.log(regions);
+              // console.log(regions);
               regions.forEach((region) => {
                 const newOption = document.createElement("OPTION");
                 newOption.setAttribute("value", region);
@@ -58,8 +54,101 @@
   else {document.addEventListener('DOMContentLoaded', fn)}
 })();
 
+document.getElementById('form-1-button').addEventListener('click', (event) => {
+  event.preventDefault();
+  const section1 = document.getElementById('section-1');
+  const pokemonId = document.getElementById('pokemon-id').value;
+  console.log(pokemonId);
+
+  const pokemonXHR = new XMLHttpRequest();
+  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`;
+  pokemonXHR.onreadystatechange = () => {
+    if (pokemonXHR.readyState === 4 && pokemonXHR.status === 200) {
+      console.log(JSON.parse(pokemonXHR.response)); 
+      const response = JSON.parse(pokemonXHR.response);
+      const pokemonCard = document.createElement("DIV");
+      const pokemonTypesWrapper = document.createElement("DIV");
+      pokemonCard.classList.add('pokemon-card');
+      section1.appendChild(pokemonCard);
+      // add class pokemon-card
+      const pokemonName = addParagraph(response.name);
+      pokemonName.classList.add('pokemon-card__name');
+      pokemonCard.appendChild(pokemonName);
+
+      const pokemonSprite = addImage(response.sprites.front_default);
+      pokemonSprite.classList.add('pokemon-card__sprite');
+      pokemonCard.appendChild(pokemonSprite);
+
+      const pokemonTypes = addParagraph('Type(s):');
+      pokemonCard.appendChild(pokemonTypes);
+      pokemonTypesWrapper.classList.add('pokemon-card__type-wrapper')
+      pokemonCard.appendChild(pokemonTypesWrapper);
+
+      const pokemonType1 = addParagraph(response.types[0].type.name);
+      pokemonType1.classList.add('pokemon-card__type');
+      pokemonTypesWrapper.appendChild(pokemonType1);
+      if (response.types.length > 1) {
+        const pokemonType2 = addParagraph(response.types[1].type.name);
+        pokemonTypesWrapper.appendChild(pokemonType2);
+      }
+
+      const pokemonMoves = addParagraph('Moves:');
+      pokemonCard.appendChild(pokemonMoves);
+      for (let i = 0; i < 4; i++) {
+        let elem = addParagraph(response.moves[i].move.name);
+        pokemonCard.appendChild(elem);
+      }
+
+
+
+      // add p tag with pokemon name
+
+      // add img tag with sprite
+
+      // add p tag with type name
+
+      // add p tag with 2nd if exists type name
+
+      // add p tag 
+
+
+
+
+      // response.name
+
+      // response.sprites.front_default
+
+      // response.types[0].type.name
+      // response.types[1].type.name
+      
+      // response.moves[0,1,2,3].move.name
+
+      // 
+
+
+      
+    }
+  }
+  pokemonXHR.open("GET", pokemonUrl, true);
+  pokemonXHR.send();
+});
+
+
+
 // on load, add types and 
 
 // /type
 
 // and regions /pokedex
+
+const addParagraph = ((content) => {
+  const p = document.createElement('p');
+  p.innerHTML = content;
+  return p;
+});
+
+const addImage = ((url) => {
+  const img = document.createElement("img");
+  img.src = url;
+  return img;
+});
