f95fa4c68b03e70583c99537ef0951943024997d
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-09-19 13:43:10 +0100

    code refactor

--- main.js
+++ main.js
@@ -58,50 +58,9 @@ document.getElementById('form-1-button').addEventListener('click', (event) => {
   const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`;
   pokemonXHR.onreadystatechange = () => {
     if (pokemonXHR.readyState === 4 && pokemonXHR.status === 200) {
-      console.log(JSON.parse(pokemonXHR.response)); 
       const response = JSON.parse(pokemonXHR.response);
-      const pokemonCard = document.createElement("DIV");
-      const pokemonTypesWrapper = document.createElement("DIV");
-      const pokemonMovesWrapper = document.createElement("DIV");
-      pokemonCard.classList.add('pokemon-card');
+      const pokemonCard = addPokemonCard(response);
       section1.appendChild(pokemonCard);
-      // add class pokemon-card
-      const pokemonName = addParagraph(response.name.toUpperCase( ));
-      pokemonName.classList.add('pokemon-card__name');
-      pokemonCard.appendChild(pokemonName);
-
-      const pokemonSprite = addImage(response.sprites.front_default);
-      pokemonSprite.classList.add('pokemon-card__sprite');
-      pokemonCard.appendChild(pokemonSprite);
-
-      const pokemonTypes = addParagraph('Type(s):');
-      pokemonTypes.classList.add('bold');
-      pokemonTypes.classList.add('margin--no');
-      pokemonCard.appendChild(pokemonTypes);
-      
-      pokemonTypesWrapper.classList.add('pokemon-card__type-wrapper')
-      pokemonCard.appendChild(pokemonTypesWrapper);
-
-      const pokemonType1 = addParagraph(response.types[0].type.name);
-      pokemonType1.classList.add('pokemon-card__type');
-      pokemonTypesWrapper.appendChild(pokemonType1);
-      if (response.types.length > 1) {
-        const pokemonType2 = addParagraph(response.types[1].type.name);
-        pokemonTypesWrapper.appendChild(pokemonType2);
-      }
-
-      const pokemonMoves = addParagraph('Moves:');
-      pokemonMoves.classList.add('bold');
-      pokemonMoves.classList.add('margin--no');
-      pokemonCard.appendChild(pokemonMoves);
-
-      pokemonMovesWrapper.classList.add('pokemon-card__move-wrapper')
-      pokemonCard.appendChild(pokemonMovesWrapper);
-      for (let i = 0; i < 4; i++) {
-        let elem = addParagraph(response.moves[i].move.name);
-        elem.classList.add('pokemon-card__move')
-        pokemonMovesWrapper.appendChild(elem);
-      }
     } else if (pokemonXHR.readyState === 4 && pokemonXHR.status === 404) {
       const warning = addParagraph('There is no Pokémon with this ID. There are 802 Pokémon. Try a different input😃');
       warning.classList.add('form__warning');
@@ -116,66 +75,24 @@ document.getElementById('form-2-button').addEventListener('click', (event) => {
   event.preventDefault();
   const section2 = document.getElementById('section-2');
   const allTypes = [];
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < document.getElementById('types-selector').options.length; i++) {
     allTypes.push(document.getElementById('types-selector').options[i].text);
   }
   toggleWarning(section2);
   toggleCard(section2);
   const input = document.getElementById('type').value;
   if (allTypes.indexOf(input.toLowerCase()) > -1) {
-    const hasType = allPokemons.filter((pokemon) => pokemon.types[0].type.name == input.toLowerCase());
-  
+    const hasType = filterByType(allPokemons, input.toLowerCase());
     const pokemonRequested = hasType[getRandomInt(hasType.length)];
 
     if (pokemonRequested) {
-      const pokemonCard = document.createElement("DIV");
-      const pokemonTypesWrapper = document.createElement("DIV");
-      const pokemonMovesWrapper = document.createElement("DIV");
-      pokemonCard.classList.add('pokemon-card');
+      const pokemonCard = addPokemonCard(pokemonRequested);
       section2.appendChild(pokemonCard);
-      // add class pokemon-card
-      const pokemonName = addParagraph(pokemonRequested.name.toUpperCase( ));
-      pokemonName.classList.add('pokemon-card__name');
-      pokemonCard.appendChild(pokemonName);
-    
-      const pokemonSprite = addImage(pokemonRequested.sprites.front_default);
-      pokemonSprite.classList.add('pokemon-card__sprite');
-      pokemonCard.appendChild(pokemonSprite);
-    
-      const pokemonTypes = addParagraph('Type(s):');
-      pokemonTypes.classList.add('bold');
-      pokemonTypes.classList.add('margin--no');
-      pokemonCard.appendChild(pokemonTypes);
-      
-      pokemonTypesWrapper.classList.add('pokemon-card__type-wrapper')
-      pokemonCard.appendChild(pokemonTypesWrapper);
-    
-      const pokemonType1 = addParagraph(pokemonRequested.types[0].type.name);
-      pokemonType1.classList.add('pokemon-card__type');
-      pokemonTypesWrapper.appendChild(pokemonType1);
-      if (pokemonRequested.types.length > 1) {
-        const pokemonType2 = addParagraph(pokemonRequested.types[1].type.name);
-        pokemonTypesWrapper.appendChild(pokemonType2);
-      }
-    
-      const pokemonMoves = addParagraph('Moves:');
-      pokemonMoves.classList.add('bold');
-      pokemonMoves.classList.add('margin--no');
-      pokemonCard.appendChild(pokemonMoves);
-    
-      pokemonMovesWrapper.classList.add('pokemon-card__move-wrapper')
-      pokemonCard.appendChild(pokemonMovesWrapper);
-      for (let i = 0; i < 4; i++) {
-        let elem = addParagraph(pokemonRequested.moves[i].move.name);
-        elem.classList.add('pokemon-card__move')
-        pokemonMovesWrapper.appendChild(elem);
-      }
     } else {
       const warning = addParagraph('Pokémon not found with this type. Please choose another from the dropdown list');
       warning.classList.add('form__warning');
       section2.appendChild(warning);
     }
-  
   } else {
     const warning = addParagraph('That is not a Pokémon Type. Please open the dropdown list. Example: Fire');
     warning.classList.add('form__warning');
@@ -184,13 +101,11 @@ document.getElementById('form-2-button').addEventListener('click', (event) => {
 });
 
 const addPokemonCard = ((pokemon) => {
-  console.log(pokemon);
-  
   const pokemonCard = document.createElement("DIV");
   const pokemonTypesWrapper = document.createElement("DIV");
   const pokemonMovesWrapper = document.createElement("DIV");
   pokemonCard.classList.add('pokemon-card');
-  const pokemonName = addParagraph(pokemon.name.toUpperCase());
+  const pokemonName = addParagraph(capFirstLetAndRemoveHyphen(pokemon.name));
   pokemonName.classList.add('pokemon-card__name');
   pokemonCard.appendChild(pokemonName);
 
@@ -206,11 +121,11 @@ const addPokemonCard = ((pokemon) => {
   pokemonTypesWrapper.classList.add('pokemon-card__type-wrapper')
   pokemonCard.appendChild(pokemonTypesWrapper);
 
-  const pokemonType1 = addParagraph(pokemon.types[0].type.name);
+  const pokemonType1 = addParagraph(capFirstLetAndRemoveHyphen(pokemon.types[0].type.name));
   pokemonType1.classList.add('pokemon-card__type');
   pokemonTypesWrapper.appendChild(pokemonType1);
   if (pokemon.types.length > 1) {
-    const pokemonType2 = addParagraph(pokemon.types[1].type.name);
+    const pokemonType2 = addParagraph(capFirstLetAndRemoveHyphen(pokemon.types[1].type.name));
     pokemonTypesWrapper.appendChild(pokemonType2);
   }
 
@@ -222,27 +137,28 @@ const addPokemonCard = ((pokemon) => {
   pokemonMovesWrapper.classList.add('pokemon-card__move-wrapper')
   pokemonCard.appendChild(pokemonMovesWrapper);
   for (let i = 0; i < 4; i++) {
-    let elem = addParagraph(pokemon.moves[i].move.name);
+    let elem = addParagraph(capFirstLetAndRemoveHyphen(pokemon.moves[i].move.name));
     elem.classList.add('pokemon-card__move')
     pokemonMovesWrapper.appendChild(elem);
   }
   return pokemonCard;
 });
 
-
-const addParagraph = ((content) => {
-  const p = document.createElement('p');
-  p.innerHTML = content;
-  return p;
+const fetchPokemonByIDToWrapper = ((wrapper, id) => {
+  const pokemonXHR = new XMLHttpRequest();
+  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+  pokemonXHR.onreadystatechange = () => {
+    if (pokemonXHR.readyState === 4 && pokemonXHR.status === 200) {
+      const pokemonCard = addPokemonCard(JSON.parse(pokemonXHR.response));
+      
+      pokemonCard.classList.remove('pokemon-card');
+      pokemonCard.classList.add('pokemon-card--small');
+      wrapper.appendChild(pokemonCard);
+    }
+  }
+  pokemonXHR.open("GET", pokemonUrl, true);
+  pokemonXHR.send();
 });
-
-const addImage = ((url) => {
-  const img = document.createElement("img");
-  img.src = url;
-  return img;
-});
-
-const getRandomInt = ((max) => Math.floor(Math.random() * Math.floor(max)));
 
 const getAllPokemons = ((counter) => {
   const pokemonXHR = new XMLHttpRequest();
@@ -262,6 +178,27 @@ const getAllPokemons = ((counter) => {
   pokemonXHR.send();
 });
 
+const addOption = ((content) => {
+  const newOption = document.createElement("OPTION");
+  newOption.setAttribute("value", content);
+  const newText = document.createTextNode(content);
+  newOption.appendChild(newText);
+  return newOption;
+});
+
+
+const addParagraph = ((content) => {
+  const p = document.createElement('p');
+  p.innerHTML = content;
+  return p;
+});
+
+const addImage = ((url) => {
+  const img = document.createElement("img");
+  img.src = url;
+  return img;
+});
+
 const toggleWarning = ((section)=> {
   if (section.contains(section.querySelector('.form__warning'))) {
     section.removeChild(section.childNodes[section.childNodes.length - 1]);
@@ -274,38 +211,13 @@ const toggleCard = ((section)=> {
   }
 });
 
-const fetchPokemonByIDToWrapper = ((wrapper, id) => {
-  const pokemonXHR = new XMLHttpRequest();
-  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${id}/`;
-  pokemonXHR.onreadystatechange = () => {
-    if (pokemonXHR.readyState === 4 && pokemonXHR.status === 200) {
-      const pokemonCard = addPokemonCard(JSON.parse(pokemonXHR.response));
-      console.log(pokemonCard);
-      
-      pokemonCard.classList.remove('pokemon-card');
-      pokemonCard.classList.add('pokemon-card--small');
-      wrapper.appendChild(pokemonCard);
-    }
-  }
-  pokemonXHR.open("GET", pokemonUrl, true);
-  pokemonXHR.send();
+const getRandomInt = ((max) => Math.floor(Math.random() * Math.floor(max)));
+
+const filterByType = ((array, input) =>   array.filter((pokemon) => pokemon.types[0].type.name == input.toLowerCase()));
+
+const capFirstLetAndRemoveHyphen = ((str) => {
+  let formattedString = str.replace(/-/, ' ');
+  return formattedString.charAt(0).toUpperCase() + formattedString.slice(1);
 });
 
-const addOption = ((content) => {
-  const newOption = document.createElement("OPTION");
-  newOption.setAttribute("value", content);
-  const newText = document.createTextNode(content);
-  newOption.appendChild(newText);
-  return newOption;
-});
-
-// do unit testing for: 
-
-// make sure that: 
-/**
- * first input is filled out
- * result is less than 806
- * first input is less than 403 and its an even number 
- */
-// unit testing for multiplication lmao 
-
+// do unit testing
