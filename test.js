45a19bf14a7ac9a066b154807fddbee647976497
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-09-19 13:42:35 +0100

    test file set up

---
+++ test.js
@@ -0,0 +1,11 @@
+var test = require('tape');
+// var fizzbuzz = require('./index.js');
+
+
+// test('Testing fizzbuzz with input string number', (t) => {
+//   let actual = fizzbuzz('15');
+//   let expected = 'Input should be a number';
+//   t.equal(actual, expected, 'Fizzbuzz should return fizzbuzz');
+//   t.end();
+// });
+
